90259c0300a729c783546288774d4aee81b19d7e
Ruby M <ranmi.chaos.fox@gmail.com>
2018-10-21 18:55:52 -0400

    Drawing the ground/color and rectangle example 💚

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ pygame.init()
 screen = pygame.display.set_mode((300, 300))
 done = False
 
+# Draw the ground
+pygame.draw.rect(screen, pygame.Color("#77AB59"), pygame.Rect(0, 290, 300, 10))
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

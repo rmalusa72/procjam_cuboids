b453ba984d9410bf4620904d4573497310314a13
Ruby M <ranmi.chaos.fox@gmail.com>
2018-10-23 13:31:42 -0400

    Fix formatting to comply with PEP

--- app/cryptid/cryptid.py
+++ app/cryptid/cryptid.py
@@ -1,13 +1,14 @@
-from app.cryptid.assets import all
+from app.cryptid.assets import DEFAULT_TORSO
 
-class Cryptid: 
+
+class Cryptid:
 
     # Color = color across all body parts
-    # Bodyparts = list of body parts. 
+    # Bodyparts = list of body parts.
     # Note that their connection structure is reflected in ports/parents, & the order of this list is arbitrary
     # Coords = 2xn array of coords for body parts, in same order as above
 
-    def __init__(color, bodyparts, coords):
+    def __init__(self, color, bodyparts, coords):
         self.color = color
         self.bodyparts = bodyparts
         self.coords = coords
@@ -18,17 +19,17 @@ class Cryptid:
 
     def makeSprite():
         # Generate sprite from bodyparts and coords
-        # To render legs & heads we are going to need to check their position relative to their parent 
+        # To render legs & heads we are going to need to check their position relative to their parent
         # (Or else store which way they are facing and update it when the creature rotates)
         return None
 
 
-class Torso: 
+class Torso:
 
     # Variant = torso sprite type
-    # Ports will point to limb or additional torso segments, or null 
- 
-    def __init__(variant = DEFAULT_TORSO, port1 = None, port2 = None, port3 = None, port4 = None, port5 = None, port6 = None): 
+    # Ports will point to limb or additional torso segments, or null
+
+    def __init__(self, variant=DEFAULT_TORSO, port1=None, port2=None, port3=None, port4=None, port5=None, port6=None):
         self.variant = variant
         self.port1 = port1
         self.port2 = port2
@@ -37,20 +38,22 @@ class Torso:
         self.port5 = port5
         self.port6 = port6
 
-class Limb: 
+
+class Limb:
 
     # Variant = limb sprite type
     # Parent points back to torso from which the limb sprouts
 
-    def __init__(variant, parent):
+    def __init__(self, variant, parent):
         self.variant = variant
         self.parent = parent
 
-class Head: 
+
+class Head:
 
     # Variant = head sprite type
     # Parent points back to torso from which head sprouts
 
-    def __init__(variant, parent):
+    def __init__(self, variant, parent):
         self.variant = variant
         self.parent = parent

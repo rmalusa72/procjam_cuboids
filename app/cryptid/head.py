7b712ef88fc57af50e2c4f07449f0e3b9ac4e2d9
Rowan McDonald <rowan@slicelife.com>
2018-10-28 13:19:06 -0400

    Add Torso#random() and simplify coords in Cryptid#randomize

--- app/cryptid/head.py
+++ app/cryptid/head.py
@@ -6,7 +6,7 @@ class Head:
     # asset_type = collection of assets that represent different permutations of the same asset
     # nose = vector from tail to head
 
-    def __init__(self, nose, asset_type=HEAD1):
+    def __init__(self, nose=NORTH, asset_type=HEAD1):
         self.asset_type = asset_type
         self.nose = nose
 
